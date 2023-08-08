b72aebc1996c7c8f064075fc96b5e8f6b6bf4025
bluecitylights <30653230+bluecitylights@users.noreply.github.com>
2023-08-08 17:39:37 +0000

    auth

---
+++ apps/groove/src/hooks.server.ts
@@ -0,0 +1,10 @@
+import { SvelteKitAuth } from "@auth/sveltekit";
+import { Spotify } from "@auth/core/providers/spotify;
+import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } from "$env/static/private";
+
+export const handle = SvelteKitAuth({
+    providers: [Spotify({
+        clientId: SPOTIFY_CLIENT_ID, 
+        clientSecret: SPOTIFY_CLIENT_SECRET
+    })],
+});
